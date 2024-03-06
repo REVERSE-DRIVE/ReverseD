673121d5a18ab0c55778b44479dcaee135b7b352
LEE SANG GYU <tkdrb062@gmail.com>
2024-03-07 08:33:42 +0900

    feat: TMP Import

---
+++ Assets/01_Scripts/SO/WeaponSO.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "WeaponSO", menuName = "ScriptableObjects/WeaponSO", order = 1)]
+public class WeaponSO : ScriptableObject
+{
+    public string weaponName;
+    public int rank;
+}
