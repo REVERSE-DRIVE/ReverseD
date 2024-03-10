910708791173616ba9619c89f0f4cf20a9f11342
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-03-10 23:57:12 +0900

    feat: MonoSingleton

---
+++ Assets/01_Scripts/InGame/Enemy/EnemyAI/EnemyAI.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EnemyManage
+{
+    
+    public class EnemyAI : MonoBehaviour
+    {
+        [SerializeField]
+        private EnemyTargetingTaget[] Targeting;
+        
+        
+    }
+}
