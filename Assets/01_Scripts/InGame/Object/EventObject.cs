f1af8dc4bc494ae1413a36d653870f9a53d37cbc
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-03-12 00:39:25 +0900

    feat: PoolManager

--- Assets/01_Scripts/InGame/Object/EventObject.cs
+++ Assets/01_Scripts/InGame/Object/EventObject.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EventObject : MonoBehaviour
+public abstract class EventObject : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] protected float lifetime;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    protected abstract void lifeRoutine();
 }
