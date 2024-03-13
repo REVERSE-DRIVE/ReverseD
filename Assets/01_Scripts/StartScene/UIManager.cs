71fea290bc43e2bf57e286149262135abd35462f
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-03-13 23:49:40 +0900

    feat: Map baseTile

--- Assets/01_Scripts/StartScene/UIManager.cs
+++ Assets/01_Scripts/StartScene/UIManager.cs
@@ -7,16 +7,11 @@ namespace StartScene
    
     public class UIManager : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
         
-        }
-
-        // Update is called once per frame
-        void Update()
+        public void OnNewProject()
         {
-        
+            
+            
         }
     } 
 }
