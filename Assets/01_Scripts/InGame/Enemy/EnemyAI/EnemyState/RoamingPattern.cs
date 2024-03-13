caa3af4acdae1cd5c99d77ed418d02db47f35bf5
LEE SANG GYU <tkdrb062@gmail.com>
2024-03-14 01:43:43 +0900

    fix: Player Move, feat: Enemy Roaming

---
+++ Assets/01_Scripts/InGame/Enemy/EnemyAI/EnemyState/RoamingPattern.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace EnemyManage
+{
+    public class RoamingPattern : EnemyAttackPattern
+    {
+        private EnemyAI _enemyAI;
+        public override void Enter()
+        {
+            base.Enter();
+        }
+
+        private void Roaming()
+        {
+            _enemyAI.StartCoroutine(RoamingCoroutine());
+        }
+
+        private IEnumerator RoamingCoroutine()
+        {
+            while (true)
+            {
+                Vector3 direction = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0);
+                _enemyAI.transform.Translate(direction * 0.1f);
+                yield return new WaitForSeconds(1f);
+            }
+        }
+
+        public override void Update()
+        {
+            base.Update();
+        }
+
+        public override void Exit()
+        {
+            base.Exit();
+        }
+    }
+}
