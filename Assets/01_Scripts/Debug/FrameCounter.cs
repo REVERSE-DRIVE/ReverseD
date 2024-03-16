0ce58fe817656ba638f59bb1666dcada71e54ddb
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-03-16 12:11:36 +0900

    test: FrameCounter

---
+++ Assets/01_Scripts/Debug/FrameCounter.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameCounter : MonoBehaviour
+{
+    private float deltaTime = 0f;
+
+    [SerializeField] private int size = 24;
+    [SerializeField] private Color color = Color.green;
+
+    void Update()
+    {
+        deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
+    }
+
+    private void OnGUI()
+    {
+        GUIStyle style = new GUIStyle();
+
+        Rect rect = new Rect(30, 30, Screen.width, Screen.height);
+        style.alignment = TextAnchor.UpperLeft;
+        style.fontSize = size;
+        style.normal.textColor = color;
+
+        float ms = deltaTime * 1000f;
+        float fps = 1.0f / deltaTime;
+        string text = string.Format("{0:0.} FPS ({1:0.0} ms)", fps, ms);
+
+        GUI.Label(rect, text, style);
+    }
+}
