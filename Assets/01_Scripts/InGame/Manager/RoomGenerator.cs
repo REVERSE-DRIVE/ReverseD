c51819bf601bac3d9738f578fadb091559784896
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-03-16 12:05:09 +0900

    feat: RoomPack

--- Assets/01_Scripts/InGame/Manager/RoomGenerator.cs
+++ Assets/01_Scripts/InGame/Manager/RoomGenerator.cs
@@ -6,126 +6,134 @@ using Random = UnityEngine.Random;
 
 namespace RoomManage
 {
-    
-public class RoomGenerator : MonoBehaviour
-{
-    [SerializeField] private Transform grid;
-    public GameObject roomPrefab; // 방 프리팹
-    public GameObject horizontalPathPrefab; // 가로로 난 길 프리팹
-    public GameObject verticalPathPrefab; // 세로로 난 길 프리팹
 
-    public int minRooms = 5; // 최소 방 개수
-    public int maxRooms = 10; // 최대 방 개수
-    public float roomSize = 17f; // 방의 가로 및 세로 길이
-    public float pathLength = 16f; // 길의 길이
-    public float HLoadXOffset;
-    public float VLoadYOffset;
-    public static Action WallGenerateEvent;
-    //private List<GameObject> rooms = new List<GameObject>(); // 생성된 방 리스트
-
-    private void Start()
+    public class RoomGenerator : MonoBehaviour
     {
-        GenerateRooms();
-        DelayAction(WallGenerateEvent, 0.1f);
-    }
+        [SerializeField] private Transform grid;
+        public RoomPack roomPack; // 방 프리팹
+        public GameObject horizontalPathPrefab; // 가로로 난 길 프리팹
+        public GameObject verticalPathPrefab; // 세로로 난 길 프리팹
 
-    public void DelayAction(Action action, float time)
-    {
-        StartCoroutine(Delay(action, time));
-    }
-    private IEnumerator Delay(Action action, float time)
-    {
-        yield return new WaitForSeconds(time);
-        action?.Invoke();
-    }
-    
+        public int minRooms = 5; // 최소 방 개수
+        public int maxRooms = 10; // 최대 방 개수
+        public float roomSize = 17f; // 방의 가로 및 세로 길이
+        public float pathLength = 16f; // 길의 길이
+        public float HLoadXOffset;
+        public float VLoadYOffset;
+        public static Action WallGenerateEvent;
+        private List<GameObject> rooms = new List<GameObject>(); // 생성된 방 리스트
 
-    private void OnDestroy()
-    {
-    }
-
-    public void GenerateRooms()
-    {
-        int numRooms = Random.Range(minRooms, maxRooms + 1);
-
-        // 초기 방 생성
-        GameObject firstRoom = Instantiate(roomPrefab, Vector3.zero, Quaternion.identity);
-        firstRoom.transform.SetParent(grid);
-        //rooms.Add(firstRoom);
-
-        // 방 연결
-        GameObject previousRoom = firstRoom;
-        for (int i = 1; i < numRooms; i++)
+        private void Start()
         {
-            GameObject newRoom = Instantiate(roomPrefab, GetNextRoomPosition(previousRoom), Quaternion.identity);
-            newRoom.transform.SetParent(grid);
-            //rooms.Add(newRoom);
-            ConnectRooms(previousRoom, newRoom);
-            previousRoom = newRoom;
+            GenerateRooms();
+            DelayAction(WallGenerateEvent, 0.1f);
         }
-    }
 
-    private Vector3 GetNextRoomPosition(GameObject previousRoom)
-    {
-        Vector3 exitDirection = GetRandomExitDirection(previousRoom);
-        return previousRoom.transform.position + exitDirection * (roomSize + pathLength);
-    }
-
-    private Vector3 GetRandomExitDirection(GameObject room)
-    {
-        Vector3[] directions = { Vector3.up, Vector3.down, Vector3.left, Vector3.right };
-        Vector3 oppositeDirection = -GetRandomExitDirection(room, directions);
-
-        Vector3 exitDirection;
-        do
+        public void DelayAction(Action action, float time)
         {
-            exitDirection = directions[Random.Range(0, directions.Length)];
-        } while (exitDirection == oppositeDirection);
+            StartCoroutine(Delay(action, time));
+        }
 
-        return exitDirection;
-    }
-
-    private Vector3 GetRandomExitDirection(GameObject room, Vector3[] directions)
-    {
-        Vector3 exitDirection = Vector3.zero;
-
-        foreach (var direction in directions)
+        private IEnumerator Delay(Action action, float time)
         {
-            Vector3 exitPosition = room.transform.position + direction * (roomSize * 0.5f + pathLength * 0.5f);
-            Collider2D[] hitColliders = Physics2D.OverlapCircleAll(exitPosition, 0.1f);
-            if (hitColliders.Length == 0)
+            yield return new WaitForSeconds(time);
+            action?.Invoke();
+        }
+
+
+        private void OnDestroy()
+        {
+        }
+
+        public void GenerateRooms()
+        {
+            int numRooms = Random.Range(minRooms, maxRooms + 1);
+
+            // 초기 방 생성
+            GameObject firstRoom = Instantiate(roomPack.rooms[0].mapPrefab, Vector2.zero, Quaternion.identity);
+            firstRoom.transform.SetParent(grid);
+            rooms.Add(firstRoom);
+
+            // 방 연결
+            GameObject previousRoom = firstRoom;
+            for (int i = 1; i < numRooms; i++)
             {
-                exitDirection = direction;
-                break;
+                GameObject newRoom = Instantiate(roomPack.RandomPickMap().mapPrefab, GetNextRoomPosition(previousRoom), Quaternion.identity);
+                newRoom.transform.SetParent(grid);
+                rooms.Add(newRoom);
+                ConnectRooms(previousRoom, newRoom);
+                previousRoom = newRoom;
             }
         }
 
-        return exitDirection;
-    }
-
-    private void ConnectRooms(GameObject room1, GameObject room2)
-    {
-        Vector3 exitDirection = GetRandomExitDirection(room1);
-        Vector3 oppositeExitDirection = -exitDirection;
-
-        Vector3 exitPosition = room1.transform.position + exitDirection * (roomSize * 0.5f);
-        Vector3 entrancePosition = room2.transform.position + oppositeExitDirection * (roomSize * 0.5f);
-
-        if (room1.transform.position.x == room2.transform.position.x) // 같은 열에 있는 경우
+        private Vector3 GetNextRoomPosition(GameObject previousRoom)
         {
-            GameObject load = Instantiate(verticalPathPrefab, (exitPosition + entrancePosition) * 0.5f + Vector3.down * 0.5f, Quaternion.identity);
-            Vector3 pos = load.transform.position;
-            load.transform.position = new Vector2(pos.x, pos.y + VLoadYOffset);
-            load.transform.SetParent(grid);
-            
+            Vector3 exitDirection = GetRandomExitDirection(previousRoom);
+            Vector3 result = previousRoom.transform.position + exitDirection * (roomSize + pathLength);
+            // for (int i = 0; i < rooms.Count; i++)
+            // {
+            //     result
+            // }
+            return result;
         }
-        else if (room1.transform.position.y == room2.transform.position.y) // 같은 행에 있는 경우
+
+        private Vector2 GetRandomExitDirection(GameObject room)
         {
-            GameObject load = Instantiate(horizontalPathPrefab, (exitPosition + entrancePosition) * 0.5f + Vector3.left * 0.5f, Quaternion.identity);
-            Vector3 pos = load.transform.position;
-            load.transform.position = new Vector2(pos.x + HLoadXOffset, pos.y);
-            load.transform.SetParent(grid);
+            Vector3[] directions = { Vector3.up, Vector3.down, Vector3.left, Vector3.right };
+            Vector2 oppositeDirection = -GetRandomExitDirection(room, directions);
+
+            Vector2 exitDirection;
+            do
+            {
+                exitDirection = directions[Random.Range(0, directions.Length)];
+            } while (exitDirection == oppositeDirection);
+
+            return exitDirection;
+        }
+
+        private Vector2 GetRandomExitDirection(GameObject room, Vector3[] directions)
+        {
+            Vector2 exitDirection = Vector3.zero;
+
+            foreach (var direction in directions)
+            {
+                Vector2 exitPosition = room.transform.position + direction * (roomSize * 0.5f + pathLength * 0.5f);
+                Collider2D[] hitColliders = Physics2D.OverlapCircleAll(exitPosition, 0.1f);
+                if (hitColliders.Length == 0)
+                {
+                    exitDirection = direction;
+                    break;
+                }
+            }
+
+            return exitDirection;
+        }
+
+        private void ConnectRooms(GameObject room1, GameObject room2)
+        {
+            Vector3 exitDirection = GetRandomExitDirection(room1);
+            Vector3 oppositeExitDirection = -exitDirection;
+
+            Vector3 exitPosition = room1.transform.position + exitDirection * (roomSize * 0.5f);
+            Vector3 entrancePosition = room2.transform.position + oppositeExitDirection * (roomSize * 0.5f);
+
+            if (room1.transform.position.x == room2.transform.position.x) // 같은 열에 있는 경우
+            {
+                GameObject load = Instantiate(verticalPathPrefab,
+                    (exitPosition + entrancePosition) * 0.5f + Vector3.down * 0.5f, Quaternion.identity);
+                Vector2 pos = load.transform.position;
+                load.transform.position = new Vector2(pos.x, pos.y + VLoadYOffset);
+                load.transform.SetParent(grid);
+
+            }
+            else if (room1.transform.position.y == room2.transform.position.y) // 같은 행에 있는 경우
+            {
+                GameObject load = Instantiate(horizontalPathPrefab,
+                    (exitPosition + entrancePosition) * 0.5f + Vector3.left * 0.5f, Quaternion.identity);
+                Vector2 pos = load.transform.position;
+                load.transform.position = new Vector2(pos.x + HLoadXOffset, pos.y);
+                load.transform.SetParent(grid);
+            }
         }
     }
-}
 }
