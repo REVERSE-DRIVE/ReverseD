e1c7709b9f0c52f622297866770fb000994ba9e3
LEE SANG GYU <tkdrb062@gmail.com>
2024-03-17 13:30:01 +0900

    feat: boss CutScene

--- Assets/01_Scripts/InGame/BossCutScene/Banner.cs
+++ Assets/01_Scripts/InGame/BossCutScene/Banner.cs
@@ -1,19 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using DG.Tweening;
 
 public class Banner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private bool isUp;
+    [SerializeField] private RectTransform targetUp;
+    [SerializeField] private RectTransform targetDown;
+    [SerializeField] private Ease ease;
+    private RectTransform rectTransform;
+
+    private void Awake()
     {
-        
+        rectTransform = GetComponent<RectTransform>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        if (isUp)
+        {
+            rectTransform.localPosition = new Vector3(-2200f, 927f);
+            rectTransform.DOLocalMove(targetUp.localPosition, 1f).SetEase(ease);
+        }
+        else
+        {
+            rectTransform.localPosition = new Vector3(2173f, -933f);
+            rectTransform.DOLocalMove(targetDown.localPosition, 1f).SetEase(ease);
+        }
     }
 }
