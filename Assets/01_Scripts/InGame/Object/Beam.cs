c2f1559c64e56f530e9c3a0529498239f54f8df0
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-03-18 18:09:39 +0900

    feat: Beam

--- Assets/01_Scripts/InGame/Object/Beam.cs
+++ Assets/01_Scripts/InGame/Object/Beam.cs
@@ -6,8 +6,15 @@ public class Beam : MonoBehaviour
     [SerializeField] private int reflection = 0;
     [SerializeField] private LineRenderer _lineRenderer;
 
+    private float time;
+
+    [SerializeField]
+    private Vector2 targetPos;
+    
     public void Reflect()
     {
-        
+        Vector2 dir = targetPos - new Vector2(transform.position.x, transform.position.y);
+        Physics2D.RaycastAll(transform.position, dir);
+
     } 
 }
