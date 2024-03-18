abb01fb2d48a5d1663c8c69531b72c2bba1b0aa4
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-03-18 11:18:40 +0900

    feat: laserProjecter

---
+++ Assets/01_Scripts/InGame/Object/Beam.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Beam : MonoBehaviour
+{
+        
+    [SerializeField] private int reflection = 0;
+    [SerializeField] private LineRenderer _lineRenderer;
+
+    public void Reflect()
+    {
+        
+    } 
+}
