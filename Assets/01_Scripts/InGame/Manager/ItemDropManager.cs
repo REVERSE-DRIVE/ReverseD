f456245cd207417f70e37877f57ae4ce8f37715f
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-03-31 14:47:31 +0900

    feat: new Tilemap Coloring, ItemDropManager

--- Assets/01_Scripts/InGame/Manager/ItemDropManager.cs
+++ Assets/01_Scripts/InGame/Manager/ItemDropManager.cs
@@ -6,18 +6,35 @@ using UnityEngine;
 
 public class ItemDropManager : MonoBehaviour
 {
-    [SerializeField] private GameObject DropItemPrefab;
-    [SerializeField] private Items _items;
-
     
+    [SerializeField] private GameObject DropItemPrefab;
+    /**
+     * <summary>
+     * 아이템 데이터 정보가 모두 저장되어있음
+     * </summary>
+     */
+    [SerializeField] private Items _items; // ItemBase
+    // 아이템 드롭 타입에 따라 다른 베이스에서 랜덤 픽하여 아이템을 드랍하기
+    
+    /**
+     * <summary>
+     * itemDropType에 따라 랜덤픽되어 아이템을 지급
+     * EX) 데이터 가드를 잡음 -> Type : Normal : 데이터 조각 / 데이터 파편 / 주소 데이터  중 1 드랍
+     * </summary>
+     */
     public GameObject DropItem(ItemDropType itemDropType, Vector2 position)
     {
         GameObject dropItem = PoolManager.Get(DropItemPrefab, position, Quaternion.identity);
-
+        // 아래 오버로딩된 DropItem을 이용해서 랜덤 드랍
 
         return dropItem;
     }
     
+    /**
+     * <summary>
+     * 실실적으로 아이템을 드랍 시키는 함수
+     * </summary>
+     */
     public GameObject DropItem(Item item, Vector2 position)
     {
         GameObject dropItem = PoolManager.Get(DropItemPrefab, position, Quaternion.identity);
