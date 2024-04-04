91e9989bcaba08fbd397cdf78023d2c8b50d6d88
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-04-04 23:40:29 +0900

    fix: Bow Sprite
    테두리 추가

--- Assets/01_Scripts/InGame/Player/Weapon/Weapon.cs
+++ Assets/01_Scripts/InGame/Player/Weapon/Weapon.cs
@@ -1,14 +1,35 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace AttackManage
 {
     public abstract class Weapon : MonoBehaviour
     {
-        [SerializeField] private int damage = 3;
-        [SerializeField] private float _attackCooltime = 1f;
-        
+        [SerializeField] protected int damage = 3;
+        [SerializeField] protected float _attackCooltime = 1f;
+
+
+        protected Animator _weaponAnimator;
+
+
+        protected void Awake()
+        {
+            if (_weaponAnimator == null)
+            {
+                _weaponAnimator.GetComponent<Animator>();
+            }
+        }
+
         public abstract void Attack();
 
-                
+        protected void AttackAnimationTrigger()
+        {
+            
+        }
+
+        protected virtual void WeaponRotate()
+        {
+            
+        }
     }
 }
