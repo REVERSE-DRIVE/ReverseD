51d65a230a954f8030e2f62755f6f4354557f047
LEE SANG GYU <tkdrb062@gmail.com>
2024-04-23 00:22:17 +0900

    refactor: TimeManager Delete OnGUI

--- Assets/01_Scripts/InGame/Manager/TimeManager.cs
+++ Assets/01_Scripts/InGame/Manager/TimeManager.cs
@@ -9,24 +9,4 @@ public class TimeManager : MonoBehaviour
     }
 
     public static float TimeScale { get; set; } = 1f;
-
-    private void OnGUI()
-    {
-        if (GUI.Button(new Rect(10, 10, 150, 100), "TimeScale 1"))
-        {
-            TimeScale = 1f;
-        }
-        if (GUI.Button(new Rect(10, 110, 150, 100), "TimeScale 0.5"))
-        {
-            TimeScale = 0.5f;
-        }
-        if (GUI.Button(new Rect(10, 210, 150, 100), "TimeScale 0.1"))
-        {
-            TimeScale = 0.1f;
-        }
-        if (GUI.Button(new Rect(10, 310, 150, 100), "TimeScale 0"))
-        {
-            TimeScale = 0f;
-        }
-    }
 }
