1cb2417056a8fb57bd8928451001e79709c10439
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-04-28 12:26:05 +0900

    feat: Skill Script Strcuture

--- Assets/01_Scripts/InGame/Player/PlayerSkillManager.cs
+++ Assets/01_Scripts/InGame/Player/PlayerSkillManager.cs
@@ -1,8 +1,71 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using SkillManage;
 using UnityEngine;
 
 public class PlayerSkillManager : MonoBehaviour
 {
+    private PlayerSkillSO _currentSkillSO;
+    public PlayerSkill skill;
+
+    private bool _isPassive;
+    private float _coolTime;
+    private float _duration;
     
+    private float _currentTime;
+    private Player _player;
+    private bool _isSkillActivated;
+    public bool IsCoolDowned => _currentTime >= _coolTime;
+
+    private void Awake()
+    {
+        _player = GetComponent<Player>();
+    }
+
+    private void ApplySkill(PlayerSkillSO skillSO)
+    {
+        _currentSkillSO = skillSO;
+        _duration = skillSO.skillDuration;
+        _coolTime = skillSO.coolTime;
+        skill = skillSO.GetSkill;
+        _isPassive = skillSO.isPassive;
+        skill.SetSkillValue(_player);
+    }
+
+    private void Update()
+    {
+        if (TimeManager.TimeScale == 0)
+            return;
+        if (_isPassive)
+        {
+            skill.UpdateSkill();
+            return;
+        }
+        _currentTime += Time.deltaTime * TimeManager.TimeScale;
+
+        if (_isSkillActivated)
+        {
+            skill.UpdateSkill();
+            if(_currentTime >= _duration)
+                EndSkill();
+        }
+    }
+
+
+    public void UseSkill()
+    {
+        if (IsCoolDowned)
+        {
+            _isSkillActivated = true;
+            _currentTime = 0;
+            skill.ActiveSkill();
+
+        }
+    }
+
+    private void EndSkill()
+    {
+        _isSkillActivated = false;
+        _currentTime = 0;
+    }
+
 }
--- Assets/01_Scripts/InGame/Player/SkillManage/PlayerSkill.cs
+++ Assets/01_Scripts/InGame/Player/SkillManage/PlayerSkill.cs
@@ -2,12 +2,42 @@
 
 namespace SkillManage
 {
-    public class PlayerSkill
+    public abstract class PlayerSkill
     {
-        [SerializeField]
-        private float _duration;
+        protected int _skillLevel;
+        protected float _skillDuration;
+        protected float _coolTime;
+        /**
+         * 스킬의 다음 사용까지의 최소 시간
+         */
+        protected float _minUseCoolTime;
+
+        protected Player _playerBase;
         
-        
+        public float SkillDuration => _skillDuration;
+        public float CoolTime => _coolTime;
+        public float MinUseCoolTime => _minUseCoolTime;
+
+        public void SetSkillValue(float duration, float coolTime, float minUseCoolTime)
+        {
+            _skillDuration = duration;
+            _coolTime = coolTime;
+            _minUseCoolTime = minUseCoolTime;
+        }
+
+        public void SetSkillValue(Player player)
+        {
+            _playerBase = player;
+        }
+
+        public virtual void ActiveSkill()
+        {
+            
+        }
+
+
+        public abstract void UpdateSkill();
+
 
     }
 }
