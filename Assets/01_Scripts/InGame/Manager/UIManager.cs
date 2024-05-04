c5bb42c2442e8db0764bde48af99eb1aa75f12c4
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-05-04 18:38:35 +0900

    fix: Rendering Manager theme Coloring

--- Assets/01_Scripts/InGame/Manager/UIManager.cs
+++ Assets/01_Scripts/InGame/Manager/UIManager.cs
@@ -111,7 +111,7 @@ namespace InGameScene
         public void ShowInfectionAlert(int infectLevel)
         { // 감염도 경고창
             InfectGaugeColor(infectLevel);
-            GameManager.Instance._RenderingManager.SetGlobalLightColor(new Color(1f,  (1 - infectLevel * 0.01f) * 0.5f + 0.5f, (1 - infectLevel * 0.01f) * 0.5f + 0.5f));
+            GameManager.Instance._RenderingManager.SetGlobalLightColor(infectLevel);
             DOTween.To(() => _infectionGauge.fillAmount, 
                 x => _infectionGauge.fillAmount = x, 
                 infectLevel * 0.01f,
