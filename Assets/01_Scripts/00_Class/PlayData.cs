f778fb022a5ca1b1a7c6dadea2187dbcbfa537bd
vcsHB <99899703+vcsHB@users.noreply.github.com>
2024-05-05 03:52:43 +0900

    feat: Resource Manager

---
+++ Assets/01_Scripts/00_Class/PlayData.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class PlayData
+{
+    public int playTime;
+    public bool isComplatete;
+}
---
+++ Assets/01_Scripts/InGame/Manager/ResourceManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class ResourceManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _text;
+    [SerializeField] private int _resourceAmount;
+
+    public int ResourceAmount => _resourceAmount;
+
+    private void Awake()
+    {
+        
+    }
+
+    public void ApplyResource(int amount)
+    {
+        _resourceAmount += amount;
+    }
+
+    // 뭔가 함수명 맘에 안드는데
+    public bool LoseResource(int amount)
+    {
+        if (amount > _resourceAmount)
+            return false;
+        _resourceAmount -= amount;
+        return true;
+    }
+
+    private void RefreshAmount()
+    {
+        _text.text = _resourceAmount.ToString();
+    }
+}
